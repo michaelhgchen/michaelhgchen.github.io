e6955c5ef48cb5a221e73904ad7c66654930c1b7
Michael Chen <michaelhgchen@gmail.com>
2018-06-18 00:13:46 -0700

    fix build environment to load corrent index.html, update positional styles

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -14,22 +14,24 @@ class App extends Component {
     } = this.props;
 
     return (
-      <div className='hero'>
-        <div>
-          <h4>{welcome}</h4>
-          <h1>{name}</h1>
-          <h4>{title}</h4>
-          <div className='link-container'>
-            {
-              links.map(({ type, url }) =>
-                <a key={type} href={url}>
-                  <i className={`link-container__icon icon icon-${type}`}/>
-                </a>
-              )
-            }
+      <main className="above-the-fold">
+        <div className="hero">
+          <div>
+            <h4>{welcome}</h4>
+            <h1>{name}</h1>
+            <h4>{title}</h4>
+            <div className="link-container">
+              {
+                links.map(({ type, url }) =>
+                  <a key={type} href={url}>
+                    <i className={`link-container__icon icon icon-${type}`}/>
+                  </a>
+                )
+              }
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     )
   }
 }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: `./src/index.js`,
@@ -27,6 +28,7 @@ module.exports = {
       {
         test: /\.(scss|css)?$/,
         use: [
+          MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
           },
@@ -50,6 +52,10 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
   ]
 };
