1baef9248c27a3436add873537080ea941e7f2ce
Michael Chen <michaelhgchen@gmail.com>
2018-06-18 02:24:56 -0700

    change theme, add picture

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,30 +7,40 @@ import './App.scss'
 class App extends Component {
   render() {
     const {
+      about,
+      image,
+      links,
       name,
       title,
       welcome,
-      links
     } = this.props;
 
     return (
-      <main className="above-the-fold">
-        <div className="hero">
-          <div>
-            <h4>{welcome}</h4>
-            <h1>{name}</h1>
-            <h4>{title}</h4>
-            <div className="link-container">
-              {
-                links.map(({ type, url }) =>
-                  <a key={type} href={url}>
-                    <i className={`link-container__icon icon icon-${type}`}/>
-                  </a>
-                )
-              }
+      <main>
+        <section className="page-half page-half--primary">
+          <div className="hero">
+            <div className="hero__image">
+              <img src={image} alt={name}/>
+            </div>
+            <div className="hero__description">
+              {welcome && <h4>{welcome}</h4>}
+              <h1>{name}</h1>
+              <h4>{title}</h4>
+              <div className="link-container">
+                {
+                  links.map(({ type, url }) =>
+                    <a key={type} href={url}>
+                      <i className={`link-container__icon icon icon-${type}`}/>
+                    </a>
+                  )
+                }
+              </div>
             </div>
           </div>
-        </div>
+        </section>
+        <section className="page-half page-half--secondary">
+          <i>{about}</i>
+        </section>
       </main>
     )
   }
